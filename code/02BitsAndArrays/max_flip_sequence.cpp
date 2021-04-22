d4c99025adfa75d7cd7901e2f21373d536082162
Hanno Barschel <hanno.barschel@uni-jena.de>
2021-04-22 16:21:58 +0200

    Every code exercise is done now, exam questions tomorow

--- code/02BitsAndArrays/max_flip_sequence.cpp
+++ code/02BitsAndArrays/max_flip_sequence.cpp
@@ -10,8 +10,20 @@
 #include <functional>
 #include <random>
 #include <iostream>
+#include <bits/stdc++.h>
 
 using namespace std;
+
+// helper macros for timings
+#define TIMERSTART(label)                                                      \
+  std::chrono::time_point<std::chrono::system_clock> a##label, b##label;       \
+  a##label = std::chrono::system_clock::now();
+
+#define TIMERSTOP(label)                                                       \
+  b##label = std::chrono::system_clock::now();                                 \
+  std::chrono::duration<double> delta##label = b##label - a##label;            \
+  std::cout << #label << ": " << std::setprecision(4) << delta##label.count()  \
+            << " seconds" << std::endl;
 
 // obfuscated code to test your solution (ignore this)
 uint32_t obfuscated(uint32_t o_abab67357a45d416943cc4921e785b98) {
@@ -85,29 +97,135 @@ uint32_t obfuscated(uint32_t o_abab67357a45d416943cc4921e785b98) {
 //   (if any) or just flipping the immediate neighboring zero. You just need to
 //   find the best choice.
 
+uint32_t count_sequence(bitset<32> bits) {
+    // returns the maximum number of concatenated Ones in the bitset
+    uint32_t seq = 0;
+    uint32_t max_seq = 0;
+    for (int i = 0; i < 32; i++) {
+        if (bits[i]){ seq++; }
+        else {
+            if (seq > max_seq){ max_seq = seq; }
+            seq = 0;
+        }
+    }
+    // important if the number ends with an 1
+    if (seq > max_seq){ max_seq = seq; }
+    return max_seq;
+}
+
+uint32_t max_flip_sequence_size_brute_force(uint32_t n) {
+    // time complexity: b^2
+    bitset<32> b = n;
+    uint32_t max_length = count_sequence(b);
+    for (int i = 0; i < 32; i++){
+        if (!b[i]){
+            // set the bit b to 1
+            b[i] = 1;
+            // Now count the sequence
+            uint32_t length = count_sequence(b);
+            if (length > max_length){
+                max_length = length;
+            }
+            // reset bit to zero
+            b[i] = 0;
+        }
+    }
+    // the time complexity is O(b^2) in worst case
+    // the space complexity is O(b)
+    return max_length;
+}
+
 uint32_t max_flip_sequence_size(uint32_t n) {
-  
-  // TODO: write code here
-  return 0;
+    bitset<32> b = n;
+    // max_length is the length of all ones in a row
+    // is reseted to 0 if we find a zero
+    uint32_t max_length = 0;
+    uint32_t length_seq = 0;
+    // counted sequence is the length of the sequence we have at the moment
+    // eventually with one bit flipped
+    uint32_t counted_seq = 0;
+    // first is true if we cant flip another bit sine we already flipped one
+    bool first = false;
+    // go through every bit
+    for (int i = 0; i < 32; i++){
+        if (!b[i]){
+            // if b[i] == 0 and first is false we just flip the 0 to 1, increment the counted sequence and
+            // set first to true since we did flip a bit
+            if (!first){
+                counted_seq++;
+                first=true;
+            }
+            else {
+                // if we already flipped a bit we have to start over
+                // if the counted sequence up to this point is more than the max sequence we
+                // replace the max sequence
+                // counted Sequence now is the sequence of ones up to this point
+                // AND the zero we flipped so it is equal to length sequence + 1
+                // finally we set the length sequence to 0 since our chain of ones is broken
+                if (counted_seq > max_length){
+                    max_length = counted_seq;
+                }
+                counted_seq = length_seq + 1;
+                first=true;
+            }
+            length_seq = 0;
+        }
+        else{
+            // if b[i] is one we just count both sequences up
+            counted_seq++;
+            length_seq++;
+        }
+    }
+    // bitset consists only of 1
+    if (counted_seq > max_length){
+        max_length = counted_seq;
+    }
+    // the time complexity is O(b)
+    // the space complexity is O(b) we can improve it by not converting the integer
+    // to an bitset
+    // TODO if enough time!
+    return max_length;
 }
 /*************** end assignment ***************/
 
 int main() {
   // test correctness
-  assert(max_flip_sequence_size(UINT32_MAX) == 32);
-  assert(max_flip_sequence_size(0) == 1);
-  assert(max_flip_sequence_size(1) == 2);
-  assert(max_flip_sequence_size(2) == 2);
-  assert(max_flip_sequence_size(UINT32_MAX - 1) == 32);
-  assert(max_flip_sequence_size(UINT32_MAX << 2u) == 31);
   int n = 100000;
   auto rand_uint32 = bind(uniform_int_distribution<uint32_t>{0, UINT32_MAX},
                           default_random_engine{std::random_device{}()});
+
+  cout << "Brute Force" << endl;
+  TIMERSTART(naive);
+  assert(max_flip_sequence_size_brute_force(UINT32_MAX) == 32);
+  assert(max_flip_sequence_size_brute_force(0) == 1);
+  assert(max_flip_sequence_size_brute_force(1) == 2);
+  assert(max_flip_sequence_size_brute_force(2) == 2);
+  assert(max_flip_sequence_size_brute_force(UINT32_MAX - 1) == 32);
+  assert(max_flip_sequence_size_brute_force(UINT32_MAX << 2u) == 31);
+
   for (int i = 0; i < n; ++i) {
     // generate a random unsigned integer
     uint32_t num = rand_uint32();
     // test if your solution is correct
     assert(max_flip_sequence_size(num) == obfuscated(num));
   }
+  TIMERSTOP(naive);
+
+  cout << "cool Solution" << endl;
+  TIMERSTART(fast);
+  assert(max_flip_sequence_size(UINT32_MAX) == 32);
+  assert(max_flip_sequence_size(0) == 1);
+  assert(max_flip_sequence_size(1) == 2);
+  assert(max_flip_sequence_size(2) == 2);
+  assert(max_flip_sequence_size(UINT32_MAX - 1) == 32);
+  assert(max_flip_sequence_size(UINT32_MAX << 2u) == 31);
+
+  for (int i = 0; i < n; ++i) {
+    // generate a random unsigned integer
+    uint32_t num = rand_uint32();
+    // test if your solution is correct
+    assert(max_flip_sequence_size(num) == obfuscated(num));
+  }
+  TIMERSTOP(fast);
   cout << "all tests passed" << endl;
 }
