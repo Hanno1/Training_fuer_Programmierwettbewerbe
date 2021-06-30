bf0ca2066bdbcc5d2a79eb1ec8c7bf5eac0160eb
Hanno Barschel <hanno.barschel@uni-jena.de>
2021-06-30 09:57:48 +0200

    Problem 459 done

--- code/04MaximumFlow/problem100.cpp
+++ code/04MaximumFlow/problem100.cpp
@@ -14,23 +14,52 @@ int compute_sol(int numb, int counter){
     }
 }
 
+int compute_big_solution(int mini, int maxi){
+    if (mini < 0 || maxi < 0){
+        cout << " Values have to be greater then 0! ";
+        return -1;
+    }
+    int ret_max = 0;
+    int solution;
+    for (int index = mini; index <= maxi; index++){
+        solution = compute_sol(index, 0);
+        if (solution > ret_max){
+            ret_max = solution;
+        }
+    }
+    return ret_max;
+}
+
 void compute_maxi(){
     int a, b;
     // later implement a map for saving computed values
     map<int, int> m;
     int solution, maxi;
     while (cin >> a >> b){
-        maxi = 0;
-        for (int index = a; index <= b; index++){
-            solution = compute_sol(index, 0);
-            if (solution > maxi){ maxi = solution; }
-        }
+        maxi = compute_big_solution(a, b);
         cout << a << " " << b << " " << maxi << "\n";
     }
 }
 
 int main(){
     // Test cases can be written here
-    compute_maxi();
+    // compute_maxi();
+    // additional Testcases
+    {
+        // same number
+        cout << "10 10 " << compute_big_solution(10, 10) << "\n";
+    }
+    {
+        // negative number
+        cout << "-1 10 " << compute_big_solution(-1, 10) << "\n";
+    }
+    {
+        // maxi < mini
+        cout << "11 8 " << compute_big_solution(11, 8) << "\n";
+    }
+    {
+        // many numbers
+        cout << "1 1000 " << compute_big_solution(1, 1000) << "\n";
+    }
     return 1;
 }
---
+++ code/04MaximumFlow/problem459.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+#include <vector>
+#include <queue>
+#include <assert.h>
+
+using namespace std;
+
+vector<int> breadth_first_search(vector<vector<int>> E, vector<int> visited, int start_node) {
+    // slightly modified breath first search. Start the algorithm at the node start_node
+    // and use the array visited
+    // return the visited array to update it
+    int n = E.size();
+    queue<int> q;
+    q.push(start_node);
+    visited[start_node] = 1;
+    int current_node;
+    while (!q.empty()) {
+        current_node = q.front();
+        q.pop();
+        for (int i=0; i<n; i++) {
+            if (E[current_node][i] > 0) {
+                if (visited[i] == 0) {
+                    q.push(i);
+                    visited[i] = 1;
+                }
+            }
+        }
+    }
+    return visited;
+}
+
+int connection_counter(int nodes_counter, vector<vector<int>> adj_mat){
+    // takes an adjazens matrix as well as the number of nodes.
+    // counter contains the number of connectivity parts
+    // visited keeps track of every visited node (gets updated in the breadth first search)
+    // if there are no nodes we want to return 0
+    // while there are zeros in visited we do a breadth search from one of the 0 nodes
+    // set them to 1, search for another zero and increment the counter
+    // if there is none we are done and return the counter
+    int counter = 0;
+    vector<int> visited(nodes_counter, 0);
+    int start = 0;
+    bool done = false;
+    if (nodes_counter == 0){ done = true; }
+    while (!done){
+        visited = breadth_first_search(adj_mat, visited, start);
+        done = true;
+        for (int i = 0; i < nodes_counter; i++){
+            if (visited[i] == 0){
+                done = false;
+                start = i;
+                break;
+            }
+        }
+        counter++;
+    }
+    return counter;
+}
+
+void task_website(){
+    int input;
+    string edge;
+    string node;
+    int node1, node2, node_counter;
+    int res = 0;
+    cin >> input;
+    cin;
+    for (int index = 0; index < input; index++){
+        cin >> node;
+        node_counter = (int)node[0] % 64;
+        vector<vector<int>> adj_mat(node_counter, vector<int> (node_counter, 0));
+        while (cin >> edge){
+            cout << edge.length() << endl;
+             if (edge.length() != 2){
+                break;
+            }
+            node1 = (int)edge[0] % 65;
+            node2 = (int)edge[1] % 65;
+            adj_mat[node1][node2] = 1;
+            adj_mat[node2][node1] = 1;
+        }
+        res = connection_counter(node_counter, adj_mat);
+        cout << "Result: " << res << endl;
+    }
+}
+
+int main(){
+    //task_website();
+    {
+        // example from the website
+        vector<vector<int>> mat = {{0,1,0,0,0},{0,1,0,1,0},{0,0,0,0,1},{0,1,0,0,0},{0,0,1,0,0}};
+        assert(connection_counter(5, mat) == 2);
+    }
+    {
+        // no nodes
+        assert(connection_counter(0, {}) == 0);
+    }
+    {
+        // 5 nodes with no connections
+        vector<vector<int>> mat = {{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0},{0,0,0,0,0}};
+        assert(connection_counter(5, mat) == 5);
+    }
+    {
+        // 5 nodes with all connections
+        vector<vector<int>> mat = {{0,1,1,1,1},{1,0,1,1,1},{1,1,0,1,1},{1,1,1,0,1},{1,1,1,1,0}};
+        assert(connection_counter(5, mat) == 1);
+    }
+    {
+        // 5 nodes with B,D,E connected, rest not
+        vector<vector<int>> mat = {{0,0,0,0,0},{0,0,0,1,1},{0,0,0,0,0},{0,1,0,0,1},{0,1,0,1,0}};
+        assert(connection_counter(5, mat) == 3);
+    }
+    {
+        // 5 nodes with B,D,E connected, and A,C
+        vector<vector<int>> mat = {{0,0,1,0,0},{0,0,0,1,1},{1,0,0,0,0},{0,1,0,0,1},{0,1,0,1,0}};
+        assert(connection_counter(5, mat) == 2);
+    }
+    {
+        // 5 nodes with B,D,E connected, rest A, c and C with E
+        vector<vector<int>> mat = {{0,0,1,0,0},{0,0,0,1,1},{1,0,0,0,1},{0,1,0,0,1},{0,1,1,1,0}};
+        assert(connection_counter(5, mat) == 1);
+    }
+    cout << "All Tests passed!" << endl;
+    return 1;
+}
